b9c42c41a735cb14040ca0ff4a7044ea3a3b4185
hi-teranishi <hi-teranishi@tgl.co.jp>
2023-07-20 16:18:24 +0900

    スクロール表示対応

--- lib/view/data_list_share.dart
+++ lib/view/data_list_share.dart
@@ -6,7 +6,6 @@ import '../model/constants.dart';
 import '../model/data_model_share.dart';
 
 class DataListShare extends StatefulWidget {
-
   const DataListShare({Key? key}) : super(key: key);
 
   @override
@@ -14,22 +13,16 @@ class DataListShare extends StatefulWidget {
 }
 
 class _DataListShare extends State<DataListShare> {
-
   List<DocumentSnapshot> documentList = [];
 
   // DBの監視用ストリーム
   final Stream<QuerySnapshot> _userStream =
       FirebaseFirestore.instance.collection(Const.collectionName).snapshots();
 
-  int id = 0;
-  String text = '';
-  String datetime = '';
-
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children:[
-        StreamBuilder<QuerySnapshot>(
+    return SingleChildScrollView(
+      child: StreamBuilder<QuerySnapshot>(
         stream: _userStream, // DB監視用のストリーム
         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
           if (snapshot.hasError) {
@@ -40,37 +33,37 @@ class _DataListShare extends State<DataListShare> {
           }
           return Column(
             children: snapshot.data!.docs.map((DocumentSnapshot document) {
-              Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
-              List<dynamic> person = data['dataList'];
-              String datetime = data['dateTime'];
-              return ListView.builder(
-                shrinkWrap: true,
-                itemCount: person.length + 1,
-                itemBuilder: (BuildContext context, int index) {
-                  if (index == 0) {
-                    return Text(datetime);
-                  }
-                  return Dismissible(
-                    key: Key(person.toString()),
-                    onDismissed: (DismissDirection direction) {
-                      setState(() {
-                        person.removeAt(index - 1);
-                        deleteData(datetime, person);
-                      });
-                    },
-                    child: ListTile(
-                      title: Text(person[index - 1]['name']),
-                      subtitle: Text(person[index - 1]['count']),
-                      leading: Text(person[index - 1]['store'])
-                    ),
-                  );
-                },
+              Map<String, dynamic> data =
+                  document.data()! as Map<String, dynamic>;
+              List<dynamic> person = data['dataList'] ?? [];
+              String datetime = data['dateTime'] ?? "なし";
+              return Column(
+                children: [
+                  Text(datetime),
+                  Column(
+                    children: person.map((dynamic unit) {
+                      return Dismissible(
+                        key: UniqueKey(),
+                        onDismissed: (DismissDirection direction) {
+                          setState(() {
+                            person.removeAt(unit);
+                            deleteData(datetime, person);
+                          });
+                        },
+                        child: ListTile(
+                            title: Text(unit['name'] ?? ""),
+                            subtitle: Text(unit['count'] ?? "0"),
+                            leading: Text(unit['store'] ?? ""),
+                            trailing: Text(unit['cost'] ?? "0")),
+                      );
+                    }).toList(),
+                  ),
+                ],
               );
             }).toList(),
           );
         },
       ),
-      ],
     );
   }
 
